9a29b003b54fcb924e867a700217c35ece754732
dionisk <33986714+dionisk@users.noreply.github.com>
2021-01-09 17:44:26 +0200

    Creating unit test project; editing "--uncatorized--" category choice in todoitems

---
+++ UniversityWebApplication/XUnitTestProject_ToDoServices_homework4/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject_ToDoServices_homework4
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
