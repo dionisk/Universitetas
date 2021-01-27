7875fe2c59f7d915f2813eca388139de6e24f575
dionisk <33986714+dionisk@users.noreply.github.com>
2021-01-27 23:54:01 +0200

    first automation test with failing localhost loading

---
+++ UniversityWebApplication/SeleniumTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace SeleniumTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void LoadLocalhost()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.google.com"); //užkrauna
+                driver.Navigate().GoToUrl("https://localhost:44310/"); //neužkrauna
+            }
+        }
+    }
+}
